81bf73fd7bfc8fef02330f3d9881332dbd792d62
Pekka Lammi <pekka@lightcognitive.com>
2018-09-25 14:22:01 +0300

    fix config

--- config.example.js
+++ config.example.js
@@ -1,6 +1,8 @@
-export const config = {
+const config = {
   TWITTER_CONSUMER_KEY: "",
   TWITTER_CONSUMER_SECRET: "",
   TWITTER_ACCESS_TOKEN_KEY: "",
   TWITTER_ACCESS_TOKEN_SECRET: ""
 };
+
+export default config;
